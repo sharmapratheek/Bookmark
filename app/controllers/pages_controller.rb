684eef913fbc78256a795cbec4d5ff383da4814f
Pratheek <sharma.pratheek59@gmail.com>
2017-12-04 10:14:54 +0530

    changes in routes show and pages_controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,5 @@
 class PagesController < ApplicationController
 	def home
-		if logged_in?
-			redirect_to bookmarks_path 
-			
-		end
+		
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "pages#home"
+  resources :users, except: [:new]
   resources :bookmarks
   get "/signup",to: "users#new"
-  resources :users, except: [:new]
+
 
   get '/login',to: "sessions#new"
   post '/login',to: "sessions#create"
   delete '/login',to: "sessions#destroy"
-  
+
 end
